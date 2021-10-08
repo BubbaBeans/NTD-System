6a11e196d6d33978c814bf95baf99227d799c1b5
Brian Hopper <brian.hopper@transdev.com>
2021-10-08 14:52:03 -0700

    Fix code smells

--- NTD-System/GlobalSuppressions.vb
+++ NTD-System/GlobalSuppressions.vb
@@ -5,16 +5,18 @@
 
 Imports System.Diagnostics.CodeAnalysis
 
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.BatchForm.RunBatchButton_Click(System.Object,System.EventArgs)")>
 <Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.MainForm.ChooseASerial(System.Collections.Generic.List{NTD_System.Route})~NTD_System.Route")>
-<Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.EnteredSurvey.StopList")>
-'<Assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.Entry.CSVtoDataSource(System.String)~System.Data.DataSet")>
-<Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.SurveyEntry.WorkingSurvey")>
-<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.BatchForm.StartButton_Click(System.Object,System.EventArgs)")>
-<Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.Entry.CSVtoDataSource(System.String,System.Windows.Forms.DataGridView@,NTD_System.EnteredSurvey@)~System.Boolean")>
 <Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.MainForm.WriteCalendar(System.String)~System.Boolean")>
-<Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.TimeOfDay(System.String,System.Int32)~System.String[]")>
+<Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.NtdSettings.WriteSettings(System.Boolean,System.String)")>
+<Assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.PlaySound(System.Boolean,System.IO.Stream)")>
 <Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.PreviouslyEntered(System.String,System.String)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.TimeOfDay(System.String,System.Int32)~System.String[]")>
+<Assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.Update_DBStops(System.Windows.Forms.DataGridView@,System.Int32)")>
+<Assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.Update_PMiles(System.Windows.Forms.DataGridView@,System.Int32)")>
+<Assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.SurveyEntry.Update_POB(System.Windows.Forms.DataGridView@,System.Int32)")>
+<Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.NtdSettings._DifferentSurvey")>
+<Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.NtdSettings._OperatingDays")>
 <Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.SurveyEntry.TOfDay")>
 <Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.SurveyEntry.TotalWorkbook")>
-<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.BatchForm.RunBatchButton_Click(System.Object,System.EventArgs)")>
-<Assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification:="<Pending>", Scope:="member", Target:="~F:NTD_System.NtdSettings._DayIndex")>
+<Assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>", Scope:="member", Target:="~M:NTD_System.Entry.CSVtoDataSource(System.String,System.Windows.Forms.DataGridView@,NTD_System.EnteredSurvey@)~System.Boolean")>
